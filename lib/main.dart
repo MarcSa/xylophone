61b8a49bb5750673f96cbc8c7c40c3014ce3fe0e
MarcSa <marc@marcvirtual.com>
2023-06-23 17:46:35 +0200

    Project ended!

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Container(),
-        ),
-      ),
-    );
+  void playSound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.wav'));
   }
+
+  Expanded buildKey({required int keyNumber, required Color keyColor}) =>
+      Expanded(
+        child: TextButton(
+          style: TextButton.styleFrom(
+            backgroundColor: keyColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(0)),
+            ),
+          ),
+          onPressed: () {
+            playSound(keyNumber);
+          },
+          child: Text(''),
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.black,
+          body: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                buildKey(keyNumber: 1, keyColor: Colors.red),
+                buildKey(keyNumber: 2, keyColor: Colors.orange),
+                buildKey(keyNumber: 3, keyColor: Colors.yellow),
+                buildKey(keyNumber: 4, keyColor: Colors.green),
+                buildKey(keyNumber: 5, keyColor: Colors.teal),
+                buildKey(keyNumber: 6, keyColor: Colors.blue),
+                buildKey(keyNumber: 7, keyColor: Colors.deepPurple),
+              ],
+            ),
+          ),
+        ),
+      );
 }
